36a1943c87b935a3b1ebc31cc372a64099fbacf5
dcrick-gmme <dcrick@gmm-enterprises.com>
2023-07-01 00:39:34 -0400

    latest code base

--- test-gmme-dotnetlib/test-gmme-dotnetlib.ps1
+++ test-gmme-dotnetlib/test-gmme-dotnetlib.ps1
@@ -1,2 +1,18 @@
+#===============================================================================
+#-- test powershell program for loading and using gmme-donetlib.dll
+#===============================================================================
+
+#-------------------------------------------------------------------------------
+#-- load .dll and output version
+#-------------------------------------------------------------------------------
 add-type -path ..\gmme-dotnetlib\bin\Debug\net7.0\gmme-dotnetlib.dll
-[GMMELib.Info]::Version()
+"GMMELib Version = " + [GMMELib.Info]::Version()
+
+
+#-------------------------------------------------------------------------------
+#-- test cmdline
+#-------------------------------------------------------------------------------
+$l_cmdline = New-Object GMMELib.Utils.CMDLine
+#("-testa")
+$l_cmdline.Dump()
+"we are here"
